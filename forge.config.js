2eb243313d7cd9395478590d55c4ada48c10fdc7
Marko Ritachka <markomarkor@gmail.com>
2023-02-20 03:34:32 -0700

    Temporarily removed notary

--- forge.config.js
+++ forge.config.js
@@ -2,12 +2,6 @@ module.exports = {
   packagerConfig: {
     icon: './images/icon',
     osxSign: {},
-    osxNotarize: {
-      tool: 'notarytool',
-      appleId: process.env.APPLE_ID,
-      appleIdPassword: process.env.APPLE_PASSWORD,
-      teamId: process.env.APPLE_TEAM_ID
-    },
   },
   rebuildConfig: {},
   makers: [
