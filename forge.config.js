402a0d0076df2c3cbe149826a035ff088992f774
Marko Ritachka <markomarkor@gmail.com>
2023-02-20 02:13:53 -0700

    Added Apple code signing

--- forge.config.js
+++ forge.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   packagerConfig: {
     icon: './images/icon',
+    osxSign: {},
+    osxNotarize: {
+      tool: 'notarytool',
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID
+    },
   },
   rebuildConfig: {},
   makers: [
